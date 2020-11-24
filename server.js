4f964fa6dcd6c1da9a9ac5669177068445490e01
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-11-24 11:49:17 -0700

    heroku deploy

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var bodyParser = require('body-parser');
 var data = require('./data');
 
 const app = express();
-const port = process.env.PORT || 3002; 
+const port = process.env.PORT || 3002;
 
 //middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-    response.send('Welcome to Image Quiz API');
-    });
+    response.send('Welcome to my Image Quiz API');
+});
 
 app.get('/quizzes', (request, response) => {
     let metadata = data.quizzes.map(x => {
-        return{ name: x.name, id: x.id, picture: x.picture }
-        });
-        response.json(metadata);
+        return { name: x.name, id: x.id, picture: x.picture }
     });
+    response.json(metadata);
+});
 
 app.get('/questions/:quizid', (request, response) => {
     let quizid = request.params.quizid;
@@ -27,18 +27,18 @@ app.get('/questions/:quizid', (request, response) => {
     if (found) {
         response.json(found.questions);
     } else {
-        response.status(404).json({error: `Quiz with id ${quizid} does not exist.`})
+        response.status(404).json({ error: `Quiz with id ${quizid} does not exist.` })
     };
-    });
+});
 
 app.post('/score', (request, response) => {
     let username = request.body.username;
     let quizid = request.body.quizid;
     let score = request.body.score;
-    data.scores.push({score: score, quizid: quizid, username: username});
-    response.json({message: `The score was added successfully.`});
-    });
+    data.scores.push({ score: score, quizid: quizid, username: username });
+    response.json({ message: `The score was added successfully.` });
+});
 
 app.listen(port, () => {
     console.log(`Image Quiz API listening on port ${port}!`);
-    });
+});
